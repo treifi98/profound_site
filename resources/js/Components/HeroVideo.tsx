e8be28a2d8ea98e598e65278e1d51d258fe69fce
treifi98 <abdullatif.treifi98@gmail.com>
2023-04-05 16:04:42 +0400

    video autopaly

--- resources/js/Components/HeroVideo.tsx
+++ resources/js/Components/HeroVideo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const HeroVideo = () => {
   return (
     <>
-        <video  controls className='h-full w-full object-cover absolute '>
+        <video  autoPlay className='h-full w-full object-cover absolute '>
             <source src="../../assets/dubai.mp4" type="video/mp4"/>
             {/* <source src="mov_bbb.ogg" type="video/ogg"/> */}
             Your browser does not support HTML video.
