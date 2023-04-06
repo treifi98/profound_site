da71c6ac090ebdd3f4477893ceb13d3ba5d1d48e
treifi98 <abdullatif.treifi98@gmail.com>
2023-04-06 13:40:44 +0400

    fix border color navelement

--- resources/js/Components/Home/Header/Search.tsx
+++ resources/js/Components/Home/Header/Search.tsx
@@ -12,12 +12,12 @@ const Search = () => {
     }
   return (
     <>
-        <div className='select-none rounded-full w-[57.55px] h-[57.55px] relative bg-[#002D31] shadow-[inset_-4px_-4px_5px_-3px_#04626B,inset_4px_4px_6px_#011618] border-[1px] border-[#1F5B62] cursor-pointer' onClick={handleClick}>
+        <div className='select-none rounded-full w-[57.55px] h-[57.55px] relative bg-[#002D31] shadow-[inset_-4px_-4px_5px_-3px_#04626B,inset_4px_4px_6px_#011618] border-[1px] border-[#1F5B62] cursor-pointer flex justify-center items-center' onClick={handleClick}>
             {/* <img src={main_search_icon} alt="" className='w-[58px] h-[58px] object-cover object-center'/> */}
             <div className='relative flex justify-center items-center'>
                 {/* <img src={clicking_indicator} alt="" className='absolute w-[51px] h-[51px] top-[7px] left-[3px]'/> */}
-                {search ? <div className='shadow-[0px_4.69775px_4.69775px_rgba(0,0,0,0.25),inset_11.7444px_11.7444px_23.4887px_rgba(5,59,66,0.16),inset_-11.7444px_-11.7444px_23.4887px_#08262C,inset_11.7444px_11.7444px_23.4887px_rgba(12,64,70,0.54)] absolute w-[50.5px] h-[50.5px] top-[3.5px] left-[3.5px] bg-[#002D31] rounded-full'></div> : ''}
-                <img src={mgg} alt="" className='absolute w-[35px] h-[35px] top-[12px] left-[12px]'/>
+                {search ? <div className='shadow-[0px_4.69775px_4.69775px_rgba(0,0,0,0.25),inset_11.7444px_11.7444px_23.4887px_rgba(5,59,66,0.16),inset_-11.7444px_-11.7444px_23.4887px_#08262C,inset_11.7444px_11.7444px_23.4887px_rgba(12,64,70,0.54)] absolute w-[50.5px] h-[50.5px]  bg-[#002D31] rounded-full flex justify-center items-center'></div> : ''}
+                <img src={mgg} alt="" className='absolute w-[35px] h-[35px] '/>
             </div>
         </div>
 
