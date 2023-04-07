cf073567213105ccb8ccb2a8d5eba05bc0508bf9
treifi98 <abdullatif.treifi98@gmail.com>
2023-04-07 15:39:02 +0400

    f

--- resources/js/Components/HeroVideo.tsx
+++ resources/js/Components/HeroVideo.tsx
@@ -15,7 +15,7 @@ const HeroVideo = () => {
                     <img src={logo_completion} alt="" className='w-[135px]'/>
                 </div>
                 <div className='absolute bg-[#01141666] z-[1] h-[calc(100vh-30px)] w-full mix-blend-overlay '></div>
-                <video  autoPlay loop className='h-[calc(100vh-30px)] w-full object-cover '>
+                <video  autoPlay loop muted className='h-[calc(100vh-30px)] w-full object-cover '>
                     <source src="../../assets/dubai.mp4" type="video/mp4"/>
                     {/* <source src="mov_bbb.ogg" type="video/ogg"/> */}
                     Your browser does not support HTML video.
