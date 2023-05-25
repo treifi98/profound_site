cc12cfd2b2175a670776c3bebd7be9bd952b4150
treifi98 <abdullatif.treifi98@gmail.com>
2023-05-25 08:10:10 +0400

    h

--- resources/js/Components/Developer/Map.tsx
+++ resources/js/Components/Developer/Map.tsx
@@ -14,7 +14,7 @@ const Map = () => {
 
           container: 'map-container',
           style: 'mapbox://styles/trevi98/cli19khgh02g701qy1jsmb8wr',
-          center: [55.2708, 25.2048], // Dubai coordinates
+          center: [55.2744, 25.2048], // Dubai coordinates
           zoom: 10,
           maxZoom:18,
           minZoom:9,
@@ -47,6 +47,87 @@ const Map = () => {
             clearLogo();
 
         });
+        map.on('load', function() {
+            map.removeLayer('poi-label');
+            // map.removeSource('poi');
+            map.addLayer({
+                "id": "points",
+                "type": "circle",
+                "paint": {
+                  "circle-radius": 20,
+                  "circle-color": '#f00'
+                },
+                "source": {
+                  "type": "geojson",
+                  "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                      "type": "Feature",
+                      "properties": {
+                        "field": 1
+                      },
+                      "geometry": {
+                        "type": "Point",
+                        "coordinates": [55.2744, 25.2048]
+                      }
+                    }]
+                  }
+                }
+              });
+            map.addLayer({
+                "id": "points1",
+                "type": "circle",
+                "paint": {
+                  "circle-radius": 20,
+                  "circle-color": '#f09'
+                },
+                "source": {
+                  "type": "geojson",
+                  "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                      "type": "Feature",
+                      "properties": {
+                        "field": 1
+                      },
+                      "geometry": {
+                        "type": "Point",
+                        "coordinates": [55.2744, 25.2148]
+                      }
+                    }]
+                  }
+                }
+              });
+          });
+          map.on('mousemove', 'points', (event) => {
+            map.getCanvas().style.cursor = 'pointer';
+            console.log(event.features[0])
+            map.setPaintProperty('points', 'circle-color', '#000');
+            // console.log(event.features[0].style = "#fff" )
+            // console.log(event.features[0].layer.paint['circle-color'].g = 'f' )
+            // console.log(event.features[0].layer.paint['circle-color'].b = 'f' )
+
+            // // Set constants equal to the current feature's magnitude, location, and time
+            // const quakeMagnitude = event.features[0].properties.mag;
+            // const quakeLocation = event.features[0].properties.place;
+            // const quakeDate = new Date(event.features[0].properties.time);
+
+            // // Check whether features exist
+            // if (event.features.length === 0) return;
+            // // Display the magnitude, location, and time in the sidebar
+            // magDisplay.textContent = quakeMagnitude;
+            // locDisplay.textContent = quakeLocation;
+            // dateDisplay.textContent = quakeDate;
+
+            // // If quakeID for the hovered feature is not null,
+            // // use removeFeatureState to reset to the default behavior
+            // if (quakeID) {
+            //   map.removeFeatureState({
+            //     source: 'earthquakes',
+            //     id: quakeID
+            //   });
+            // alert("s")
+            })
 
 
         // Clean up on component unmount
