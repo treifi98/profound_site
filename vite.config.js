a253eb516ead1d5916d7275e0f4704d2c1ecba35
abdullatif <abdullatif.treifi@gmail.com>
2023-06-17 01:04:19 +0400

    f

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import purgecss from 'vite-plugin-purgecss'
+    import { defineConfig } from 'vite';
+    import laravel from 'laravel-vite-plugin';
+    import react from '@vitejs/plugin-react';
+    // import purgecss from 'vite-plugin-purgecss'
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.jsx',
-            refresh: true,
-        }),
-        react(),
-        purgecss()
-    ],
-});
+    export default defineConfig({
+        plugins: [
+            laravel({
+                input: 'resources/js/app.tsx',
+                ssr: 'resources/js/ssr.jsx',
+                refresh: true,
+            }),
+            react()
+        ],
+    });
