edd0b0578171c1f86daa32815ded816078e7fea8
abdullatif <abdullatif.treifi@gmail.com>
2023-07-04 12:15:00 +0400

    f

--- resources/js/Components/Developer/PartLogoTextOGRL.tsx
+++ resources/js/Components/Developer/PartLogoTextOGRL.tsx
@@ -44,12 +44,12 @@ const PartLogoTextOGRL = (props:Props) => {
             ):
             (
                 <div className='w-[375px] h-max flex items-center justify-center bg-[#E6EDED] shadow-[6.286655426025391px_6.286655426025391px_12.573310852050781px_0px_#BABFBF,-6.286655426025391px_-6.286655426025391px_12.573310852050781px_3.1433277130126953px_#FFF] mobil-comp'>
-                    <div className='w-[325px] h-max flex flex-col-reverse gap-[0px] mx-auto my-[25px]'>
+                    <div className='w-[335px] h-max flex flex-col-reverse gap-[0px] mx-auto my-[20px]'>
 
-                        <div className='h-full w-[325px] border-[#DCE3E3] border-[0.0625rem] textElementShadow rounded-[0.9822875rem] box-border py-[1rem] flex justify-center items-center '>
+                        <div className='h-full w-[335px] border-[#DCE3E3] border-[0.0625rem] textElementShadow rounded-[0.9822875rem] box-border py-[1rem] flex justify-center items-center '>
 
 
-                            <div className='w-[325px] mx-auto '>
+                            <div className='w-[335px] mx-auto '>
                                 <div className='font-[600] text-grade text-[1.25rem] w-[94%] mx-auto'>
                                     Latest Projects By Emaar
                                 </div>
@@ -59,11 +59,11 @@ const PartLogoTextOGRL = (props:Props) => {
                                 </div>
                             </div>
                         </div>
-                        <div className='w-[325px] h-[20.60056rem] flex justify-center items-center mb-[25px]'>
-                            <div className='w-[325px] h-[20.60056rem] flex justify-center items-center rounded-[33.93%_3.33%_33.93%_2.22%] border-[0.0625rem] border-[#DCE3E3] shadow-[inset_-0.294195625rem_-0.294195625rem_0.58839125rem_0.1470975rem_#FFFFFF,inset_0.294195625rem_0.294195625rem_0.58839125rem_#BABFBF]'>
-                                <div className='w-[18.89313rem] h-[19.22719rem] rounded-[33.93%_3.33%_33.93%_2.22%] shadow-[2.8838510513305664px_2.8838510513305664px_5.767702102661133px_0px_#BABFBF,-2.8838510513305664px_-2.8838510513305664px_5.767702102661133px_1.4419255256652832px_#FFF]'>
+                        <div className='w-[335px] h-[339.60896px] flex justify-center items-center mb-[20px]'>
+                            <div className='w-[335px] h-[339.60896px] flex justify-center items-center rounded-[33.93%_3.33%_33.93%_2.22%] border-[0.0625rem] border-[#DCE3E3] shadow-[inset_-0.294195625rem_-0.294195625rem_0.58839125rem_0.1470975rem_#FFFFFF,inset_0.294195625rem_0.294195625rem_0.58839125rem_#BABFBF]'>
+                                <div className='w-[312.29008px] h-[317.63504px] rounded-[33.93%_3.33%_33.93%_2.22%] shadow-[2.8838510513305664px_2.8838510513305664px_5.767702102661133px_0px_#BABFBF,-2.8838510513305664px_-2.8838510513305664px_5.767702102661133px_1.4419255256652832px_#FFF]'>
 
-                                    <img src={dubai} alt="" className='w-[18.89313rem] h-[19.22719rem] rounded-[33.93%_3.33%_33.93%_2.22%]'/>
+                                    <img src={dubai} alt="" className='w-[312.29008px] h-[317.63504px] rounded-[33.93%_3.33%_33.93%_2.22%]'/>
                                 </div>
                             </div>
                         </div>
